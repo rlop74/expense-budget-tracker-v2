12403ba227515c8dfb24eacee037c5e87d6f024e
rlop74 <119091421+rlop74@users.noreply.github.com>
2025-11-17 09:21:10 -0500

    fixed sidebar to show layout properly between smaller and bigger screen (not responsive yet)

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {
     ArrowLeftRight,
     LayoutDashboard,
@@ -14,6 +15,10 @@ import {
 } from "lucide-react";
 
 export const Sidebar = ({ showSidebar, setShowSidebar }) => {
+    const [theme, setTheme] = useState(
+        localStorage.getItem("theme") || <Sun />
+    );
+
     const menuItems = [
         { icon: <LayoutDashboard />, name: "Dashboard" },
         { icon: <ArrowLeftRight />, name: "Transactions" },
@@ -24,51 +29,57 @@ export const Sidebar = ({ showSidebar, setShowSidebar }) => {
         { icon: <Settings />, name: "Settings" },
     ];
 
+    const themeItems = [<Sun />, <Moon />];
+
     return (
         <div
-            className={`flex flex-col p-5 list-none bg-violet-500/30 transition-all duration-300 ease-in-out ${
-                showSidebar ? " w-[12%]" : "w-0"
+            className={`flex flex-col justify-between p-5 list-none bg-violet-500/30 transition-all duration-300 ease-in-out ${
+                showSidebar ? " w-[15%] translate-x-0" : "w-0 -translate-x-50"
             }`}
         >
-            <div
-                className={`transition-all duration-300 ease-in-out ${
-                    showSidebar ? "translate-x-0" : "-translate-x-50"
-                }`}
-            >
+            <div className="flex flex-col text-m w-[100%] gap-1">
                 <div className="flex text-[20px] pt-5 pb-5">
                     <Vault className="" />
                     Budget Tracker
                 </div>
-                <div className="flex flex-col h-full justify-between">
-                    <div className="flex flex-col text-m w-[100%] gap-1 pb-30">
-                        {menuItems.map((item) => {
-                            return (
-                                <button className="flex gap-2 p-4 rounded-l-full rounded-r-full">
-                                    {item.icon}
-                                    {item.name}
-                                </button>
-                            );
-                        })}
-                    </div>
+                {menuItems.map((item) => {
+                    return (
+                        <button
+                            key={item.name}
+                            className="flex gap-2 p-4 rounded-l-full rounded-r-full"
+                        >
+                            {item.icon}
+                            {item.name}
+                        </button>
+                    );
+                })}
+            </div>
 
-                    <div className="flex flex-col text-m gap-1">
-                        <button className="flex gap-2 p-4 rounded-l-full rounded-r-full">
-                            <MessageCircleQuestionMark />
-                            Help
-                        </button>
-                        <button className="flex gap-2 p-4 rounded-l-full rounded-r-full">
-                            <LogOut />
-                            Logout
-                        </button>
-                        <div className="mt-20 flex rounded-l-full rounded-r-full border-1 border-gray-400 w-fit">
-                            <button className="rounded-full p-3">
-                                <Sun />
+            <div className="flex flex-col text-m gap-20">
+                <div>
+                    <button className="flex gap-2 p-4 rounded-l-full rounded-r-full">
+                        <MessageCircleQuestionMark />
+                        Help
+                    </button>
+                    <button className="flex gap-2 p-4 rounded-l-full rounded-r-full">
+                        <LogOut />
+                        Logout
+                    </button>
+                </div>
+                <div className="flex rounded-l-full rounded-r-full border-1 border-gray-400 w-fit">
+                    {themeItems.map((themeItem) => {
+                        return (
+                            <button
+                                className={`rounded-full p-3 ${
+                                    theme === themeItem
+                                        ? "bg-violet-500/30"
+                                        : ""
+                                }`}
+                            >
+                                {themeItem}
                             </button>
-                            <button className="rounded-full p-3">
-                                <Moon />
-                            </button>
-                        </div>
-                    </div>
+                        );
+                    })}
                 </div>
             </div>
         </div>
