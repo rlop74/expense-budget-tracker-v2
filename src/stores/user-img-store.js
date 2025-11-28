2e12fc8e166d2a61f5b553523d04e454648339e8
rlop74 <119091421+rlop74@users.noreply.github.com>
2025-11-28 00:21:53 -0500

    added user-img to public folder and updated user-img-store.js to have that img

--- src/stores/user-img-store.js
+++ src/stores/user-img-store.js
@@ -1,7 +1,7 @@
 import { create } from 'zustand'
 
 export const useUserImg = create((set) => ({
-    userImg: "https://media.licdn.com/dms/image/v2/D4E03AQGy1OWBIfOy2A/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1691031045223?e=1764201600&v=beta&t=O2Nwj1howzF6UzghUIPliAHVF8_z0qfA3KVrEAACU4s",
+    userImg: "../public/user-img.png",
     changeUserImg: (img) =>
         set((state) => ({
             userImg: img,
