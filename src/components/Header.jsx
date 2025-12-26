07e86818301df3278580a4d5ef71720bcff628a7
rlop74 <119091421+rlop74@users.noreply.github.com>
2025-12-25 23:59:46 -0500

    fixed header styling on dark mode

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,7 @@ export const Header = () => {
             <div className="flex justify-between px-5 py-6">
                 {/* left side, greeting */}
                 <div className="">
-                    <h1 className="text-4xl font-bold text-gray-900">
+                    <h1 className="text-4xl font-bold">
                         Welcome back, {user?.first_name || "User"}!
                     </h1>
                     <p className="mt-1 text-gray-500">
@@ -23,36 +23,37 @@ export const Header = () => {
                 <div className="flex items-center gap-1">
                     {/* search bar */}
                     <div
-                        className={`flex items-center rounded-full border border-gray-400 p-3 bg-white text-gray-500 cursor-pointer select-none hover:bg-violet-500/50 hover:text-white transition-all duration-300 ease-in-out ${
+                        className={`flex items-center rounded-full border border-gray-400 p-3 cursor-pointer select-none hover:bg-violet-500/50 hover:text-white transition-all duration-300 ease-in-out ${
                             isSearchOpen ? "w-74 gap-1" : "w-12"
                         }`}
                     >
                         <Search
                             onClick={() => setIsSearchOpen(!isSearchOpen)}
+                            className="text-gray-500"
                         />
                         <input
                             type="text"
                             placeholder="Search transactions, accounts..."
                             className={`rounded-full outline-none transition-all duration-350 ease-in-out ${
-                                isSearchOpen ? "w-full text-black" : "w-0"
+                                isSearchOpen ? "w-full" : "w-0"
                             }`}
                         />
                     </div>
 
                     {/* notifications */}
-                    <button className="flex items-center rounded-full border border-gray-400 p-3 bg-white text-gray-500 cursor-pointer hover:bg-violet-500/50 hover:text-white transition-all duration-300 ease-in-out">
+                    <button className="flex items-center rounded-full border border-gray-400 p-3 text-gray-500 cursor-pointer hover:bg-violet-500/50 hover:text-white transition-all duration-300 ease-in-out">
                         <Bell />
                     </button>
 
                     {/* user profile */}
-                    <button className="flex items-center gap-3 rounded-full border border-gray-400 bg-white px-4 py-2 cursor-pointer transition hover:border-violet-400 hover:shadow-sm">
+                    <button className="flex items-center gap-3 rounded-full border border-gray-400 px-4 py-2 cursor-pointer transition hover:border-violet-400 hover:shadow-sm">
                         <img
                             src={user?.img}
                             alt="User avatar"
                             className="h-10 w-10 rounded-full"
                         />
                         <div className="hidden text-left sm:block">
-                            <div className="text-sm font-medium text-gray-900">
+                            <div className="text-sm font-medium">
                                 {user?.first_name} {user?.last_name}
                             </div>
                             <div className="text-xs text-gray-500">
